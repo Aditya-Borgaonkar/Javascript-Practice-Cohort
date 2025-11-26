974828552f4ce5a555cd2e47e71f6dc61653c275
Aditya Borgaonkar <adityaborgaonkar4@gmail.com>
2025-11-26 11:34:13 +0530

    Change img

--- Json-data/script.js
+++ Json-data/script.js
@@ -3,28 +3,28 @@ let main=document.querySelector('.main')
 const users = [
     {
         fullName: "Amit Barge",
-        image: "https://randomuser.me/api/portraits/men/11.jpg",
+        image: "https://plus.unsplash.com/premium_photo-1707932495000-5748b915e4f2?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         profession: "Web Developer",
         description: "Frontend specialist with a passion for smooth UI and modern web trends.",
         button: "Follow"
     },
     {
         fullName: "Sneha Patil",
-        image: "https://randomuser.me/api/portraits/women/21.jpg",
+        image: "https://images.unsplash.com/photo-1604514628550-37477afdf4e3?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         profession: "UI/UX Designer",
         description: "Creates minimal, user-focused designs and enjoys prototype building.",
         button: "Follow"
     },
     {
         fullName: "Rahul Sharma",
-        image: "https://randomuser.me/api/portraits/men/32.jpg",
+        image: "https://images.unsplash.com/photo-1604771968376-e6f1f321be5d?q=80&w=769&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         profession: "Software Engineer",
         description: "Enjoys solving complex problems and writing clean, scalable code.",
         button: "Follow"
     },
     {
         fullName: "Priya Kulkarni",
-        image: "https://randomuser.me/api/portraits/women/41.jpg",
+        image: "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         profession: "Digital Marketer",
         description: "Focused on SEO, branding, and building strong digital presence.",
         button: "Follow"
