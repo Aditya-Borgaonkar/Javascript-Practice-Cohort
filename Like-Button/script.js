5eccc632d16a655c5317bb8ab8f6a8876ca6ff17
Aditya Borgaonkar <adityaborgaonkar4@gmail.com>
2025-11-27 14:53:26 +0530

    Done

--- Like-Button/script.js
+++ Like-Button/script.js
@@ -1,9 +1,22 @@
-let img=document.querySelector('img');
-let love=document.querySelector('i');
+let img = document.querySelector('img');
+let love = document.querySelector('i');
 
-img.addEventListener('dblclick',function(){
-    console.log('Hello');
-     love.style.opacity=1
-     love.style.transform= 'tra'
-    
+img.addEventListener('dblclick', function () {
+    love.style.opacity = 1
+    love.style.transform = ' translate(-50%,-50%) scale(1) rotate(0deg)'
+
+    setTimeout(() => {
+        
+        love.style.transform = ' translate(-50%,-300%) scale(1) rotate(60deg)'
+    },800);
+    setTimeout(() => {
+        love.style.opacity = 0;
+       
+    },1000);
+    setTimeout(() => {
+         love.style.transform = ' translate(-50%,-50%) scale(0) rotate(0deg)'
+    }, 1200);
+
+
+
 })
